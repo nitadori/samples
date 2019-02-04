4fb24c979b4376db25a417ec4777232e8757119c
Toshiki Teramura <toshiki.teramura@gmail.com>
2019-02-04 13:51:56 +0900

    Format

--- 1_Utilities/deviceQuery/main.cpp
+++ 1_Utilities/deviceQuery/main.cpp
@@ -12,39 +12,40 @@
 #include <stdexcept>
 #include <vector>
 
-int main() {
-  // Init platform
-  std::vector<cl::Platform> platforms;
-  cl::Platform::get(&platforms);
-  if (platforms.size() == 0) {
-    throw std::runtime_error("No platform found");
-  }
-  cl::Platform plat = platforms[0];
-  std::clog << "Using platform: " << plat.getInfo<CL_PLATFORM_NAME>()
-            << std::endl;
+int main()
+{
+    // Init platform
+    std::vector<cl::Platform> platforms;
+    cl::Platform::get(&platforms);
+    if (platforms.size() == 0) {
+        throw std::runtime_error("No platform found");
+    }
+    cl::Platform plat = platforms[0];
+    std::clog << "Using platform: " << plat.getInfo<CL_PLATFORM_NAME>()
+              << std::endl;
 
-  // Init device
-  std::vector<cl::Device> devs;
-  plat.getDevices(CL_DEVICE_TYPE_ALL, &devs);
-  if (devs.size() == 0) {
-    throw std::runtime_error("No devices found");
-  } else {
-    std::clog << devs.size() << " device(s) found" << std::endl;
-  }
+    // Init device
+    std::vector<cl::Device> devs;
+    plat.getDevices(CL_DEVICE_TYPE_ALL, &devs);
+    if (devs.size() == 0) {
+        throw std::runtime_error("No devices found");
+    } else {
+        std::clog << devs.size() << " device(s) found" << std::endl;
+    }
 
-  // Query device info
-  for (size_t i = 0; i < devs.size(); ++i) {
+    // Query device info
+    for (size_t i = 0; i < devs.size(); ++i) {
+        std::clog << "-------------------------------------------" << std::endl;
+        std::clog << "Device ID    : " << i << std::endl;
+        cl::Device dev = devs[i];
+        std::clog << "Name         : " << dev.getInfo<CL_DEVICE_NAME>()
+                  << std::endl;
+        std::clog << "Vender       : " << dev.getInfo<CL_DEVICE_VENDOR>()
+                  << std::endl;
+        std::clog << "Max Work Item: "
+                  << dev.getInfo<CL_DEVICE_MAX_WORK_ITEM_SIZES>()[0] << std::endl;
+    }
     std::clog << "-------------------------------------------" << std::endl;
-    std::clog << "Device ID    : " << i << std::endl;
-    cl::Device dev = devs[i];
-    std::clog << "Name         : " << dev.getInfo<CL_DEVICE_NAME>()
-              << std::endl;
-    std::clog << "Vender       : " << dev.getInfo<CL_DEVICE_VENDOR>()
-              << std::endl;
-    std::clog << "Max Work Item: "
-              << dev.getInfo<CL_DEVICE_MAX_WORK_ITEM_SIZES>()[0] << std::endl;
-  }
-  std::clog << "-------------------------------------------" << std::endl;
 
-  return 0;
+    return 0;
 }
